f756488a881c725f5185ec9bc4de2dc6c085ae29
baardl <bard.lind@gmail.com>
2020-08-25 10:17:57 +0200

    #14 Improve readablillity of test

--- src/test/java/no/entra/bacnet/json/objects/ObjectIdBuilderTest.java
+++ src/test/java/no/entra/bacnet/json/objects/ObjectIdBuilderTest.java
@@ -12,10 +12,22 @@ class ObjectIdBuilderTest {
     @Test
     void build() {
         //objectId c40200000b
-        OctetReader octetReader = new OctetReader("00000b");
-        ObjectId objectId = new ObjectIdBuilder(Octet.fromHexString("02")).withInstanceNumberOctet(octetReader.nextOctets(3)).build();
+        OctetReader octetReader = new OctetReader("0200000b");
+        Octet objectIdType= octetReader.next(); //02
+        ObjectId objectId = new ObjectIdBuilder(objectIdType).withInstanceNumberOctet(octetReader.nextOctets(3)).build();
         assertNotNull(objectId);
         assertEquals(ObjectType.AnalogValue, objectId.getObjectType());
         assertEquals("11", objectId.getInstanceNumber());
+    }
+
+    @Test
+    void analogInput0() {
+        //objectId 1c00000000
+        OctetReader octetReader = new OctetReader("00000000");
+        Octet objectIdType= octetReader.next(); //02
+        ObjectId objectId = new ObjectIdBuilder(objectIdType).withInstanceNumberOctet(octetReader.nextOctets(3)).build();
+        assertNotNull(objectId);
+        assertEquals(ObjectType.AnalogInput, objectId.getObjectType());
+        assertEquals("0", objectId.getInstanceNumber());
     }
 }
